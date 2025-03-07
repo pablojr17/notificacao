a27545a3ad7eadd2ef86f5ff6a8335521a5a4714
pablojr17 <pablojr17@gmail.com>
2025-03-07 00:00:17 -0300

    add metodos de envio de email

---
+++ src/main/java/com/pablo/notificacao/busines/dto/TarefasDTO.java
@@ -0,0 +1,27 @@
+package com.pablo.notificacao.busines.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.pablo.notificacao.busines.enums.StatusNotificacaoEnum;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class TarefasDTO {
+
+    private String id;
+    private String nomeTarefa;
+    private String descricao ;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    private LocalDateTime dataCriacao;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    private LocalDateTime dataEvento;
+    private String emailUsuario;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    private LocalDateTime dataAlteracao;
+    private StatusNotificacaoEnum statusNotificacaoEnum;
+}
